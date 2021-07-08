42dfc3916076baaeb20b3bee16c95183d963c120
Richa Bhaskarwar <richabhaskarwar@richapattewar.local>
2021-07-08 07:21:20 +0530

    Fixed extension upgrade issue by Amit

--- extension/yt/main.js
+++ extension/yt/main.js
@@ -1,8 +1,12 @@
 function showButton(video_id) {
   let $title_elem = $('h1.title.ytd-video-primary-info-renderer');
-  console.log($title_elem);
-  let button_html = "<a class='nammi-btn' target='_blank' href='https://mink.ilostin.com/welcome.html?youtube_id="+video_id+"'>WatchOnMink</a>"
-  $title_elem.append(button_html)
+  // console.log($title_elem);
+  let button_html = "<a class='nammi-btn' target='_blank' href='https://mink.ilostin.com/welcome.html?youtube_id="+video_id+"'>WatchOnMink</a>";
+  // let button_html = "<a class='nammi-btn' target='_blank' href='https://www.google.com/'>Nammi</a>";
+  let $button = $(button_html).appendTo($title_elem).on("click mouseover", function(e) {
+    let video_id = getYoutubeVideoID();
+    e.target.href = "https://mink.ilostin.com/welcome.html?youtube_id="+video_id+"";
+  });
 }
 
 (function() {
@@ -12,6 +16,6 @@ function showButton(video_id) {
     console.log(video_id);
     setTimeout(function() {
       showButton(video_id);
-    }, 100);
+    }, 250);
   }
 })();
