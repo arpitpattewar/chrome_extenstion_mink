5a505c3c0cca4741b1c48bea3e848452cad362ad
Riyaz Khan Pathan <66507064+RiyazKhanPathan@users.noreply.github.com>
2021-08-20 16:39:55 +0530

    added ContextMenu Script

---
+++ extension/script.js
@@ -0,0 +1,23 @@
+const contextMenuId = "myMenuId";
+
+function openLink(info,tab) {
+  if (info.menuItemId !== contextMenuId) {
+    return;
+  }
+  let url = info.linkUrl;
+  if(!url){
+    url = info.pageUrl;
+
+  }
+
+   chrome.tabs.create({  
+     url: "https://mink.ilostin.com/welcome.html?youtube_id="+url.replace("https://www.youtube.com/watch?v=","")
+   });
+}
+chrome.contextMenus.create({
+  title: "open with player", 
+  contexts:["link","page"], 
+  documentUrlPatterns: ["https://www.youtube.com/*"],
+  id: contextMenuId
+});
+chrome.contextMenus.onClicked.addListener(openLink)
